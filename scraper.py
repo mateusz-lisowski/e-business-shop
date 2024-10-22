22a2617c7e892cae23582bbbe58c9e4a5441134d
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-10-22 19:48:18 +0200

    Establish base scraper scaffold

---
+++ scraper.py
@@ -0,0 +1,17 @@
+BASE_URL = 'https://shop.poohcorner.co.uk/'
+
+
+def extract_main_page():
+    pass
+
+
+def extract_product():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

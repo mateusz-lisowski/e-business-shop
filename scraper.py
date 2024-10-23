e4c381a2687a6745187bce275847094571de0a33
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-10-23 23:50:06 +0200

    Implement x6 faster async based scraper version

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 import time
+import asyncio
 from decimal import Decimal
 from uuid import uuid4
 from pathlib import Path
 
-import requests
+import aiofiles
+import httpx
 from bs4 import BeautifulSoup
 from price_parser import Price
 from pydantic import BaseModel
@@ -12,6 +14,9 @@ from pydantic import BaseModel
 BASE_URL = 'https://shop.poohcorner.co.uk/'
 OUTPUT_DIR = Path('output')
 IMAGE_OUTPUT_DIR = OUTPUT_DIR / 'images'
+
+limits = httpx.Limits(max_keepalive_connections=5, max_connections=10)
+client = httpx.AsyncClient(timeout=10.0, limits=limits)
 
 
 class Product(BaseModel):
@@ -36,25 +41,28 @@ class Categories(BaseModel):
     categories: list[Category]
 
 
-def save_photo(url: str) -> str:
-    response = requests.get(f'https:{url}')
+async def fetch_page(url: str) -> BeautifulSoup:
+    response = await client.get(url)
+    response.raise_for_status()
+    return BeautifulSoup(response.text, 'lxml')
+
+
+async def save_photo_async(url: str) -> str:
+    response = await client.get(f'https:{url}')
 
     if response.status_code != 200:
         raise ValueError(f"Wrong status code {response.status_code} while requesting image")
 
     filename = f'{IMAGE_OUTPUT_DIR}/{uuid4()}.png'
-    with open(filename, 'wb') as image_file:
-        image_file.write(response.content)
+
+    async with aiofiles.open(filename, 'wb') as image_file:
+        await image_file.write(response.content)
 
     return filename
 
 
-def extract_product(sub_url: str) -> Product:
-    response = requests.get(f'{BASE_URL}{sub_url}')
-    if response.status_code != 200:
-        raise ValueError(f"Wrong status code {response.status_code} while requesting product page")
-
-    page = BeautifulSoup(response.text, 'lxml')
+async def extract_product(sub_url: str) -> Product:
+    page = await fetch_page(f'{BASE_URL}{sub_url}')
 
     product_name = page.find('h1').text
     product_description_container = page.find('div', class_='product-short-description rte')
@@ -63,7 +71,8 @@ def extract_product(sub_url: str) -> Product:
 
     product_description = product_description_container.get_text()
     product_price = Price.fromstring(product_price_str)
-    image_src = save_photo(product_image_url)
+
+    image_src = await save_photo_async(product_image_url)
 
     product = Product(
         name=product_name,
@@ -73,77 +82,71 @@ def extract_product(sub_url: str) -> Product:
         images=[image_src]
     )
 
-    print(f"Extracted product: {product}")
-
     return product
 
 
-def extract_subcategory(sub_url: str) -> Subcategory:
-    response = requests.get(f'{BASE_URL}{sub_url}')
-    if response.status_code != 200:
-        raise ValueError(f"Wrong status code {response.status_code} while requesting subcategory page")
-
-    page = BeautifulSoup(response.text, 'lxml')
+async def extract_subcategory(sub_url: str) -> Subcategory:
+    page = await fetch_page(f'{BASE_URL}{sub_url}')
 
     subcategory_name = page.find('h1').text
     products = page.find_all('product-card')
     products_urls: list[str] = [p.figure.a['href'] for p in products]
 
+    products_list = await asyncio.gather(*(extract_product(url) for url in products_urls))
+
     subcategory = Subcategory(
         name=subcategory_name,
-        products=[extract_product(url) for url in products_urls]
+        products=products_list
     )
-
-    print(f"Successfully extracted all products from subcategory {subcategory_name}")
 
     return subcategory
 
 
-def extract_categories() -> list[Category]:
-    categories: list[Category] = []
-
-    response = requests.get(BASE_URL)
+async def extract_categories() -> list[Category]:
+    response = await client.get(BASE_URL)
     if response.status_code != 200:
         raise ValueError(f"Wrong status code {response.status_code} while requesting main page")
 
     page = BeautifulSoup(response.text, 'lxml')
 
     html_categories = page.find_all('li', class_='menu-item-has-children')
+    categories: list[Category] = []
 
     for html_category in html_categories:
         category_name = html_category.a.text.strip()
         html_subcategories = html_category.ul.find_all('a')
         subcategories_urls: list[str] = [s['href'] for s in html_subcategories]
+
+        subcategories_list = await asyncio.gather(*(extract_subcategory(url) for url in subcategories_urls))
+
         category = Category(
             name=category_name,
-            subcategories=[extract_subcategory(url) for url in subcategories_urls]
+            subcategories=subcategories_list
         )
-        print(f"Successfully extracted all subcategories from category {category_name}")
         categories.append(category)
 
     return categories
 
 
-def main():
+async def main():
     start_time = time.time()
 
     OUTPUT_DIR.mkdir(exist_ok=True)
     IMAGE_OUTPUT_DIR.mkdir(exist_ok=True)
 
     categories = Categories(
-        categories=extract_categories()
+        categories=await extract_categories()
     )
-
-    print(categories)
 
     filename = f'{OUTPUT_DIR}/output.json'
     with open(filename, 'w', encoding='utf-8') as json_file:
         json_file.write(categories.model_dump_json())
 
     end_time = time.time()
-
     elapsed_time = end_time - start_time
     print(f"Execution time: {elapsed_time:.5f} seconds")
 
+
 if __name__ == '__main__':
-    main()
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # Set event loop policy (Windows only)
+    asyncio.run(main())

d7767b4008494473d597b0df0d18dc75f881d5aa
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-11-30 00:14:39 +0100

    Remove webdriver type hints

--- tests/main.py
+++ tests/main.py
@@ -1,47 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 
 ROOT_URL = 'http://localhost:8080/gb/'
 
 
-def test_add_products_to_cart(driver: WebDriver):
+def test_add_products_to_cart(driver):
+    category1 = driver.get_element(By.CLASS_NAME, "information")
+
+
+def test_search_by_product_name_and_add_random_to_cart(driver):
     pass
 
 
-def test_search_by_product_name_and_add_random_to_cart(driver: WebDriver):
+def test_remove_products_from_cart(driver):
     pass
 
 
-def test_remove_products_from_cart(driver: WebDriver):
+def test_register_new_account(driver):
     pass
 
 
-def test_register_new_account(driver: WebDriver):
+def test_order_cart_contents(driver):
     pass
 
 
-def test_order_cart_contents(driver: WebDriver):
+def test_order_with_payment_method(driver):
     pass
 
 
-def test_order_with_payment_method(driver: WebDriver):
+def test_chose_one_carrier(driver):
     pass
 
 
-def test_chose_one_carrier(driver: WebDriver):
+def test_accept_order(driver):
     pass
 
 
-def test_accept_order(driver: WebDriver):
+def test_check_order_status(driver):
     pass
 
 
-def test_check_order_status(driver: WebDriver):
-    pass
-
-
-def test_download_vat_invoice(driver: WebDriver):
+def test_download_vat_invoice(driver):
     pass
 
 

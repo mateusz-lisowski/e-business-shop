02dd50cf941478a6203588e9f435376969f31069
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-11-30 00:06:05 +0100

    Implement test functions scaffolding

--- tests/main.py
+++ tests/main.py
@@ -1,15 +1,52 @@
-import time
-
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 
 ROOT_URL = 'http://localhost:8080/gb/'
 
 
+def test_add_products_to_cart(driver: WebDriver):
+    pass
+
+
+def test_search_by_product_name_and_add_random_to_cart(driver: WebDriver):
+    pass
+
+
+def test_remove_products_from_cart(driver: WebDriver):
+    pass
+
+
+def test_register_new_account(driver: WebDriver):
+    pass
+
+
+def test_order_cart_contents(driver: WebDriver):
+    pass
+
+
+def test_order_with_payment_method(driver: WebDriver):
+    pass
+
+
+def test_chose_one_carrier(driver: WebDriver):
+    pass
+
+
+def test_accept_order(driver: WebDriver):
+    pass
+
+
+def test_check_order_status(driver: WebDriver):
+    pass
+
+
+def test_download_vat_invoice(driver: WebDriver):
+    pass
+
+
 def main():
     driver = webdriver.Chrome()
-    driver.get(ROOT_URL)
-    time.sleep(10)
     driver.quit()
 
 

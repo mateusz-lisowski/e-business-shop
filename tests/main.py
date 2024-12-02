447ca4351d1b4cb81f648c6b010894c84491f467
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-02 22:54:29 +0100

    Add web driver wait for waiting

--- tests/main.py
+++ tests/main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 ROOT_URL = 'http://localhost:8080/pl/'
